b442f33801ff89f075bc0ec61c3ea7304a7044d1
Sergey Storchay <r8@r8.com.ua>
2012-12-14 08:29:37 +0200

    Added head block

---
+++ app/code/community/Magemaven/Background/Block/Head.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * @category    Magemaven
+ * @package     Magemaven_Background
+ * @copyright   Copyright (c) 2011-2012 Sergey Storchay <r8@r8.com.ua>
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+class Magemaven_Background_Block_Head extends Mage_Core_Block_Template
+{
+}
